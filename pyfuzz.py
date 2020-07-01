5f3421e1811bb17e54ac4128f7138c1427f270ba
Aditya Arole <aditya090300@gmail.com>
2020-07-01 12:14:59 +0530

    finished standard port scanning and web dir fuzzing

---
+++ pyfuzz.py
@@ -0,0 +1,218 @@
+# Python3-based web application directory & file fuzzer.
+# Copyright (C) 2020  Aditya Arole
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Import required libraries
+import argparse
+import queue
+import requests
+import sys
+import threading
+
+
+# Create the Fuzzer class
+class Fuzzer:
+	# Define the constructor for the class
+	def __init__(self,base,codes,extensions,output,dirs,threads):
+		# Set all attributes using the arguments provided
+		self.base = base
+		self.codes = codes
+		self.extensions = extensions
+		self.output = output
+		self.dirs = dirs
+		self.threads = threads
+
+		# Set the wordlist resume point to None
+		self.resume = None
+
+		# Append a / to the base URL if not provided
+		if self.base[-1] != "/":
+			self.base += "/"
+
+
+	# Define the method to return a queue of names
+	def make_queue(self):
+		# Create a boolean to check if resume point is found
+		found = False
+		# Create a queue to store the dirs
+		word_queue = queue.Queue()
+		
+		# Iterate over the directories
+		for word in self.dirs:
+			# Execute block if the resume point is not none
+			if self.resume is not None:
+				# If the resume point is found, add the word to the queue
+				if found:
+					word_queue.put(word)
+				# Else if the word to resume from is the same as the current word,
+				# set resume point to true and inform the user
+				else:
+					if self.resume == word:
+						found = True
+						print(f"Resuming from {self.resume}")
+			# If no resume word is set, add the current word to the queue
+			else:
+				word_queue.put(word)
+
+		# Return the queue
+		return word_queue
+
+
+	# Define the method to bruteforce the dir discovery
+	def brute(self, word_queue):
+		# Execute the following block till the supplied queue is not empty
+		while not word_queue.empty():
+			# Get the item at the front of the queue and create a list to brute it
+			testdir = word_queue.get()
+			testlist = list()
+
+			# Append the item as a dir and as a file (using the extensions provided)
+			testlist.append(f"{testdir}/")
+			for ext in self.extensions:
+				testlist.append(f"{testdir}.{ext}")
+
+			# Attempt all items from the new list
+			for attempt in testlist:
+				# Append the item to the base URL
+				testurl = self.base + attempt
+				# Create a variable to store the response code
+				code = 100
+
+				# Try to make a GET request and store the code
+				try:
+					code = requests.get(testurl).status_code
+
+					# If the code is acceptable, create a string containing the code and the attempt
+					if code in self.codes:
+						line = f"{code} -> {attempt}"
+						# If -o flag is not used, print to STDOUT
+						if self.output is None:
+							print(line)
+						# Else, write result to the specified file
+						else:
+							self.output.write(f"{line}\n")
+				# Catch any exception and inform the user
+				except Exception as e:
+					print(f"Error ({attempt}): {str(e)}")
+
+
+	# Define the start point for all objects of the class
+	def run(self):
+		# Create a word queue
+		word_queue = self.make_queue()
+
+		# Spawn threads and start bruteforce discovery of dirs and files
+		# Number of threads specified using the -t flag (if not present, default = 10)
+		for _ in range(self.threads):
+			new_thread = threading.Thread(target=self.brute,args=(word_queue,))
+			new_thread.start()
+		
+
+# Define a method to parse command line arguments
+def define_args():
+	# Create a usage string
+	usage_str = "python3 pyfuzz.py [-w /path/to/wordlist.txt (or) -n 100-1000] -u http://target.url:[port]/ [-o file] [-c 100,200,300] [-e xml,json]"
+	
+	# Create an ArgumentParser object
+	parser = argparse.ArgumentParser(description="PyFuzz by Aditya Arole (@e1ora)",usage=usage_str)
+	
+	# Add required arguments
+	parser.add_argument("-u","--url",dest="url",type=str,metavar="url",help="URL to fuzz; default port = 80")
+	parser.add_argument("-w","--wordlist",dest="wordlist",type=str,metavar="wordlist",help="Path to wordlist text file")
+	parser.add_argument("-o","--output",dest="output",type=str,metavar="output",help="Write results to a file; if not specified, results are printed to STDOUT")
+	parser.add_argument("-c","--codes",dest="codes",type=str,metavar="codes",help="Acceptable return codes, comma separated (default list: 200,301,403)")
+	parser.add_argument("-e","--extensions",dest="extensions",type=str,metavar="extensions",help="Acceptable file extensions, comma separated (default list: php,html,js,txt)")
+	parser.add_argument("-t","--threads",dest="threads",type=int,metavar="threads",help="Number of threads (default: 10)")
+	parser.add_argument("-n","--numeric",dest="numeric",type=str,metavar="num_range",help="Search for numeric in a certain range (format: -n 100-1000; disabled by default)")
+	
+	# Set defaults for the arguments
+	parser.set_defaults(codes="200,301,403",extensions="php,html,js,txt",threads=10,numeric="")
+
+	# If no arguments provided, print help message and exit
+	if not len(sys.argv) > 1:
+		parser.print_help()
+		exit()
+	
+	# Return parsed arguments
+	return parser.parse_args(sys.argv[1:])
+
+
+# Define the driver function for the program
+def main():
+	# Get the command line arguments
+	args = define_args()
+
+	# Store the arguments in appropriate variables
+	# Store acceptable codes as a list fo integers
+	codes = args.codes.split(",")
+	codes = [int(x) for x in codes]
+	
+	# Store the file extensions as a list of strings
+	extn = args.extensions.split(",")
+	
+	# Store the output file
+	out_file = args.output
+	
+	# Create a list to store the contents of the wordlist
+	names = list()
+
+	# Open the output file, if specified
+	if out_file is None:
+		pass
+	else:
+		out_file = open(out_file,"w")
+
+	# If the -n flag is used, create a wordlist of numbers
+	# Else, use the wordlist file
+	lb, ub = 0, 0
+	if len(args.numeric):
+		# Split the argument and store the two values
+		lb = int(args.numeric.split()[0])
+		ub = int(args.numeric.split()[1]) + 1
+		# Append the integers to the list as strings
+		for i in range(lb, ub):
+			names.append(str(i))
+	else:
+		# Open the wordlist file
+		with open(args.wordlist, "r") as wordlist:
+			# Read the lines from the wordlist
+			lines = wordlist.readlines()
+
+			# Iterate over the lines
+			for line in lines:
+				# Strip the line
+				line = line.strip()
+
+				# If the line is neither a comment nor empty,
+				# append the contents to the list of names
+				if line[0:1] == "#" or len(line) == 0:
+					continue
+				else:
+					names.append(line)
+
+	# Create an instance of the Fuzzer class using the variables defined above
+	fuzz = Fuzzer(args.url, codes, extn, out_file, names, args.threads)
+	# Run the program
+	fuzz.run()
+		
+
+# Execute the main function if the pyfuzz.py is run
+if __name__ == "__main__":
+	try:
+		main()
+	# Exit the program if a KeyboardInterrupt is received
+	except KeyboardInterrupt:
+		print("Interrupt received. Exiting.")
+		sys.exit(0)
---
+++ recon.py
@@ -0,0 +1,109 @@
+import argparse
+import nmap
+import os
+import pyfuzz
+import subprocess
+import sys
+
+
+class Machine:
+	def __init__(self, ip, name, dir):
+		self.ip = ip
+		self.name = name
+		self.base_dir = os.getcwd()
+		self.dir = f"{dir}/{name}"
+
+		try:
+			os.mkdir(self.dir)
+		except FileExistsError:
+			pass
+		os.chdir(self.dir)
+
+		self.port_scan(1024)
+
+	
+	def port_scan(self, upper_bound):
+		scanner = nmap.PortScanner()
+		ports_to_scan = f"1-{upper_bound}"
+		
+		scanner.scan(self.ip, ports=ports_to_scan, arguments="-sC -sV")
+
+		base_dict = scanner._scan_result['scan'][self.ip]['tcp']
+
+		with open(f"{self.name}.nmapout", "w") as outfile:
+			for port in base_dict.keys():
+				outfile.write(f"Port: {port}\n")
+				for detail in base_dict[port].keys():
+					outfile.write(f" {detail}: ")
+					test_var = base_dict[port][detail]
+					
+					if type(test_var) is not dict:
+						outfile.write(f"{test_var}\n")
+					else:
+						for key in test_var.keys():
+							outfile.write(f"\n  {key}: ")
+							if test_var[key].count("\n") > 0:
+								outfile.write(f"{test_var[key]}")
+							else:
+								outfile.write(f"{test_var[key]}")
+				outfile.write("\n\n")
+
+		if scanner[self.ip].has_tcp(80):
+			self.fuzz_web_server()
+
+	
+	def fuzz_web_server(self):
+		url = f"http://{self.ip}/"
+		
+		names = list()
+		# Open the wordlist file
+		with open(f"{self.base_dir}/dirs.txt", "r") as wordlist:
+			# Read the lines from the wordlist
+			lines = wordlist.readlines()
+
+			# Iterate over the lines
+			for line in lines:
+				# Strip the line
+				line = line.strip()
+
+				# If the line is neither a comment nor empty,
+				# append the contents to the list of names
+				if line[0:1] == "#" or len(line) == 0:
+					continue
+				else:
+					names.append(line)
+		
+		fuzzer = pyfuzz.Fuzzer(base=url,codes=[200,301,403],extensions=["php","html","js","txt"],output=open(f"{self.name}.fuzzout", "w"),dirs=names,threads=10)
+		fuzzer.run()
+
+
+# Create a method to get the arguments from the command line
+def define_args():
+	# Define the usage string
+	usage_str = """python3 recon.py -i/--ip MACHINE_IP -n/--name MACHINE_NAME -w/--working-directory WORKING_DIRECTORY
+	Example: python3 recon.py -i 10.10.10.191 -n Blunder -w /home/user/htb"""
+	# Create an ArgumentParser object
+	parser = argparse.ArgumentParser(description="Recon script by Aditya Arole (@e1ora)",usage=usage_str)
+	# Add the -i, -n and -w arguments
+	parser.add_argument("-i","--ip",dest="ip",type=str,metavar="IP",help="IP address of the target machine")
+	parser.add_argument("-n","--name",dest="name",type=str,metavar="NAME",help="Name of the target machine")
+	parser.add_argument("-w","--working-directory",dest="wd",type=str,metavar="WORKING_DIRECTORY",help="Path to working directory (files will be stored in a subdirectory here)")
+	#parser.add_argument("-l","--wordlist",dest="wordlist",type=str,metavar="WORDLIST",help="Wordlist to use while fuzzing")
+
+	# If no arguments are provided, print the help message and exit
+	if not len(sys.argv) > 1:
+		parser.print_help()
+		exit()
+	
+	# Return the command line arguments
+	return parser.parse_args(sys.argv[1:])
+
+
+def main():
+	args = define_args()
+
+	Machine(args.ip, args.name, args.wd)
+
+
+if __name__ == "__main__":
+	main()
